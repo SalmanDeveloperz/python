90a7c4743bbcd81ece3e67d44ccc3c8cc95e9c9e
Muhammad Salman <151463017+SalmanDeveloperz@users.noreply.github.com>
2024-09-06 13:17:20 +0500

    Learning List features in Python

---
+++ list.py
@@ -0,0 +1,13 @@
+list=[1,2,3,4,5]
+#list functions
+list.append(6)  #To add element in list
+list.remove(5)  #To remove element from the list
+list.clear()    #To clear the list 
+print(list)
+
+
+#Concatention in List
+list1=[1,2,3,4,5,6,7,8]
+list2=['Salman', 'Ali', 'Hamza', 'Kashif', 'Faizan']
+newList=list1+list2     #This will conacatenate both of two list's indexes
+print(newList)
