785745bf1e8c6b074578f08e939e99abf6b18eed
Muhammad Salman <151463017+SalmanDeveloperz@users.noreply.github.com>
2024-09-06 10:01:41 +0500

    Updated 4 programs withon one first program to show clearity

--- firstProgramInPython.py
+++ firstProgramInPython.py
@@ -1,20 +1,24 @@
+#Program # 1:
 i=32
 print("Hy I'm Chaudhary Salman",i)
+
+#Program # 2: 
 #CALCULATE AREA OF RECTANGLE
 length =32 # this is length we used
 width =12 #this is width we used
-
 Area= length* width
 print("The Area is: Area= length * width =>>", length,"*",width,"=",Area)
+print(type(Area)) # To show the type of varibale Output)=>>>>> String
 
-print(type(Area))
-
-print('I am good today')
-myAgre=21
+# Program # 3:
+#Embedding Value Method
+myAge=21
 myName='I am Muhammad Salman, A University Student'
 myMessage=('%s I am %s years old')
-print(myMessage %(myName, myAgre))
+print(myMessage %(myName, myAge))
 
+#Program # 4:
+# F String Method
 Age=21
 Name='I am Muhammad Salman, A University Student'
 Message= f'{Name}, I am {Age} years old and have a CS Enrollment in University of Agriculture Faisalabad'
