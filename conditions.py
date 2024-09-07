1a62b6661a13fc50db54062465512256724a4837
Muhammad Salman <151463017+SalmanDeveloperz@users.noreply.github.com>
2024-09-07 08:40:59 +0500

    Learning conditions and unlocking new features of python

---
+++ conditions.py
@@ -0,0 +1,16 @@
+Rs= 100
+if Rs < 200:
+    print('I am not there')
+if Rs==100:
+    print('I am fine')
+
+else:
+    print('I am there')
+
+
+#combining conditions in python
+money=30
+if money == 10 or money ==20 or money ==30:
+    print('I am so so so happay with learning python ')
+else:
+    print('I am fine and happy too ')
