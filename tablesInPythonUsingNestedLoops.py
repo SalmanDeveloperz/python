66cd51f4581020d755b64b8248f3d08f22b95cd6
Muhammad Salman <151463017+SalmanDeveloperz@users.noreply.github.com>
2024-09-08 08:14:09 +0500

    Edit file of tables created using loops and nested loops

--- tablesInPythonUsingNestedLoops.py
+++ tablesInPythonUsingNestedLoops.py
@@ -1,21 +1,18 @@
 # num=int(input('Enter the number whose table you want to print:'))
-
-# # Simple Tables
+# # Simple Tables Method 1
 # for j in range (1,11):
 #     print(num,' x ',j,' = ',num*j)
 #     # j+=1
 # print('\n')
 
+# #Simple Method 2:
+# num=2
+# for j in range (1,11):
+#     print(f'{num} X {j} = {num*j}')
 
-#Print Tbles of 1 to 10 in one time (Very Easy and Efficient)
+
+#Print Tables of 1 to 10 in one time (Very Easy and Efficient)
 for i in range(1,11):
     for j in range (1,11):
         print(f'{i} X {j} = {i*j}')
     print('================')
-
-#multiplication Table 
-
-# num1=10
-# num2=20
-# ans=num1*num2
-# print(ans)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+#Print Tbles of 1 to 10 in one time (Very Easy and Efficient)
+# for i in range(1,11):
+#     for j in range (1,11):
+#         print(f'{i} X {j} = {i*j}')
+#     print('================')
+
+# #multiplication Table 
